405c2436ca4ec21f506404f390d5ae5dcb635e0f
Barbara Vitoria <104398217+BarbaraSts@users.noreply.github.com>
2022-05-03 01:47:52 -0300

    Add files via upload
    Laço de repetição

---
+++ src/main/ExerEstruturaDeRepeticao.java
@@ -0,0 +1,19 @@
+package exerestruturaderepeticao.exerestruturaderepeticao;
+
+/**
+ *
+ * @author Barbara
+ */
+public class ExerEstruturaDeRepeticao {
+
+    public static void main(String[] args) {
+        /* Faça um programa que mostre todos os números inteiros de 1 a 50. Use o laço WHILE. */
+             
+        int numerosInteiros = 1;
+        while(numerosInteiros <= 50){
+            System.out.printf("%d \n", numerosInteiros);
+            numerosInteiros++;
+        }
+    }
+}
+                             // FEITO //
